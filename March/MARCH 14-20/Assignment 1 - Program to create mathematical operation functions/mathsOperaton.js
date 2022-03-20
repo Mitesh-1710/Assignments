1cd118c14c4cc00ee91239c7cc5563f09224ae48
Mitesh Mayavanshi <82865550+Mitesh-1710@users.noreply.github.com>
2022-03-20 11:37:12 +0530

    Add files via upload

---
+++ March/MARCH 14-20/Assignment 1 - Program to create mathematical operation functions/mathsOperaton.js
@@ -0,0 +1,25 @@
+function addtion(a,b){
+    return a + b;
+}
+
+function minus(a,b){
+    return a - b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function multipy(a,b){
+    return a * b;
+}
+
+function square(a,b){
+    return (a + b)**2;
+}
+
+console.log("3 + 4 = " , addtion(3,4));
+console.log("10 - 4 = " , minus(10,4));
+console.log("8 / 2 = " , divide(8,2));
+console.log("2.5 * 10 = " , multipy(2.5,10));
+console.log("2 ^ 10 = " , square(2,10));
