fdd86e55c18cfe17d79bba4409392018f630300a
Mitesh Mayavanshi <82865550+Mitesh-1710@users.noreply.github.com>
2022-03-20 12:59:47 +0530

    Add files via upload

---
+++ March/MARCH 14-20/Assignment 2 - Program to implement fibonacci series/fibonacci.js
@@ -0,0 +1,12 @@
+//Program To Implement Fibonacci Series
+let a = 0;
+let b = 1;
+console.log(a);
+console.log(b);
+for(i = 1; i<=20 ; i++)
+{   
+    let result = a + b;
+    console.log(result);
+    a = b;
+    b = result;
+}
